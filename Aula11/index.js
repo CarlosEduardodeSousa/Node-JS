7bb95f56e4af30418180ef690c9f235a3b675c06
CarlosEduardodeSousa <carloseduardosousa378@gmail.com>
2026-09-22 16:19:53 -0300

    BodyParser

--- Aula11/index.js
+++ Aula11/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const port = 8081
 const { engine } = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 
 //Config
     //Template Engine 
         app.engine('handlebars', engine({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
+    //Body Parser
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
     //Conexão com o banco de dados MySql
         const Sequelize = require('sequelize')
         const sequelize = new Sequelize('teste', 'user', 'password',{
@@ -22,7 +26,7 @@ app.get('/cad', function(req, res){
 
 /*Envidando dados do formulario*/
 app.post('/add', function(req, res){ 
-    res.send('Formulario recebido!')
+    res.send(`Titulo: ${req.body.titulo}, Conteúdo: ${req.body.conteudo}`)
 })
 
 
